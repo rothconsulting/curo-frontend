b2107f77d5826cdd8f009b737884797f1d9020b3
Dominik Forster <domi@imod.ch>
2020-11-05 08:26:56 +0100

    feat: add missing exports

--- frontend/packages/core/src/public-api.ts
+++ frontend/packages/core/src/public-api.ts
@@ -2,4 +2,17 @@
  * Public API Surface of core
  */
 
+export * from './lib/camunda/attachment';
+export * from './lib/camunda/camunda-base-path';
+export * from './lib/camunda/comment';
+export * from './lib/camunda/count-result';
+export * from './lib/camunda/form';
+export * from './lib/camunda/identity-link';
+export * from './lib/camunda/sort-task-query-parameters';
+export * from './lib/camunda/task';
+export * from './lib/camunda/task-query';
+export * from './lib/camunda/task/task.service';
+export * from './lib/camunda/variable-query-parameter';
+export * from './lib/camunda/variable-value';
+export * from './lib/camunda/variables';
 export * from './lib/core.module';
