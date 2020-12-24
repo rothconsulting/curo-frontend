779df93479618f26b80363879c16ac43ca5728ab
Christof Haller <christof.haller@umb.ch>
2020-12-24 12:05:39 +0100

    feat: add demo process without curo but with generated task forms

--- backend/demo/src/main/kotlin/ch/umb/curo/demo/delegates/InformSoftwareArchitectDelegate.kt
+++ backend/demo/src/main/kotlin/ch/umb/curo/demo/delegates/InformSoftwareArchitectDelegate.kt
@@ -1,5 +1,7 @@
 package ch.umb.curo.demo.delegates
 
+import ch.umb.curo.demo.camunda.CamundaVariables
+import ch.umb.solutions.curo.libraries.sharedprocess.camunda.CamundaVariableHelper
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.slf4j.LoggerFactory
@@ -10,6 +12,14 @@ class InformSoftwareArchitectDelegate : JavaDelegate {
     private val logger = LoggerFactory.getLogger(InformSoftwareArchitectDelegate::class.java)
 
     override fun execute(delegateExecution: DelegateExecution?) {
-        logger.info("Running current activity: " + (delegateExecution!!.currentActivityName))
+        logger.info("Current activity: " + (delegateExecution!!.currentActivityName) + "...")
+
+        val camundaVariableHelper = CamundaVariableHelper(delegateExecution)
+        val title = camundaVariableHelper[CamundaVariables.TITLE]
+        val category = camundaVariableHelper.getOrNull(CamundaVariables.CATEGORY)
+        val description = camundaVariableHelper.getOrNull(CamundaVariables.DESCRIPTION)
+        val url = camundaVariableHelper.getOrNull(CamundaVariables.URL)
+
+        logger.info("Send suggestion '$title' to software architect...")
     }
 }
--- backend/demo/src/main/kotlin/ch/umb/curo/demo/delegates/SaveSuggestionDelegate.kt
+++ backend/demo/src/main/kotlin/ch/umb/curo/demo/delegates/SaveSuggestionDelegate.kt
@@ -1,16 +1,30 @@
 package ch.umb.curo.demo.delegates
 
+import ch.umb.curo.demo.camunda.CamundaVariables
+import ch.umb.solutions.curo.libraries.sharedprocess.camunda.CamundaVariableHelper
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.slf4j.LoggerFactory
 import javax.inject.Named
 
 @Named("saveSuggestion")
-class SaveSuggestionDelegate : JavaDelegate{
+class SaveSuggestionDelegate : JavaDelegate {
     private val logger = LoggerFactory.getLogger(SaveSuggestionDelegate::class.java)
 
     override fun execute(delegateExecution: DelegateExecution?) {
-        logger.info("Running current activity: " + (delegateExecution!!.currentActivityName))
+        logger.info("Current activity: " + (delegateExecution!!.currentActivityName) + "...")
+
+        val camundaVariableHelper = CamundaVariableHelper(delegateExecution)
+        val title = camundaVariableHelper[CamundaVariables.TITLE]
+        val category = camundaVariableHelper.getOrNull(CamundaVariables.CATEGORY)
+        val description = camundaVariableHelper.getOrNull(CamundaVariables.DESCRIPTION)
+        val url = camundaVariableHelper.getOrNull(CamundaVariables.URL)
+
+        logger.info("\tSave:")
+        logger.info("\t\tTitle: $title")
+        logger.info("\t\tCategory: $category")
+        logger.info("\t\tDescription: $description")
+        logger.info("\t\tURL: $url")
     }
 }
 
