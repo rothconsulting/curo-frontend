b0aea8bcb24e661411bdfc92f197406f7776f5b2
Dominik Forster <domi@imod.ch>
2021-01-04 11:59:33 +0100

    fix: change task assign http method

--- frontend/packages/core/src/public-api.ts
+++ frontend/packages/core/src/public-api.ts
@@ -6,5 +6,6 @@ export * from './lib/core.module';
 export * from './lib/curo-base-path';
 export * from './lib/curo-form-accessor';
 export * from './lib/generic-form/generic-form.component';
+export * from './lib/process/process.service';
 export * from './lib/task/task';
 export * from './lib/task/task.service';
