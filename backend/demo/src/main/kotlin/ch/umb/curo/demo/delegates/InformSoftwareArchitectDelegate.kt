ce76bdf15ac0b078d3849d6b71cf5faca880e684
Christof Haller <christof.haller@umb.ch>
2021-01-07 13:38:22 +0100

    feat: add camunda variables to delegates

--- backend/demo/src/main/kotlin/ch/umb/curo/demo/delegates/InformSoftwareArchitectDelegate.kt
+++ backend/demo/src/main/kotlin/ch/umb/curo/demo/delegates/InformSoftwareArchitectDelegate.kt
@@ -3,22 +3,18 @@ package ch.umb.curo.demo.delegates
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 import javax.inject.Named
 
-@Named("informSoftwareArchitect")
+@Component("informSoftwareArchitect")
 class InformSoftwareArchitectDelegate : JavaDelegate {
     private val logger = LoggerFactory.getLogger(InformSoftwareArchitectDelegate::class.java)
 
     override fun execute(delegateExecution: DelegateExecution?) {
         logger.info("Current activity: " + (delegateExecution!!.currentActivityName) + "...")
 
-        //TODO - Christof: as soon as we can retrieve Camunda variable you can uncomment the lines
-//        val camundaVariableHelper = CamundaVariableHelper(delegateExecution)
-//        val title = camundaVariableHelper[CamundaVariables.TITLE]
-//        val category = camundaVariableHelper.getOrNull(CamundaVariables.CATEGORY)
-//        val description = camundaVariableHelper.getOrNull(CamundaVariables.DESCRIPTION)
-//        val url = camundaVariableHelper.getOrNull(CamundaVariables.URL)
-//
-//        logger.info("Send suggestion '$title' to software architect...")
+        val title = delegateExecution.getVariable("title")
+
+        logger.info("Send suggestion '$title' to software architect...")
     }
 }
--- backend/demo/src/main/kotlin/ch/umb/curo/demo/delegates/SaveSuggestionDelegate.kt
+++ backend/demo/src/main/kotlin/ch/umb/curo/demo/delegates/SaveSuggestionDelegate.kt
@@ -3,27 +3,28 @@ package ch.umb.curo.demo.delegates
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 import javax.inject.Named
 
-@Named("saveSuggestion")
+@Component("saveSuggestion")
 class SaveSuggestionDelegate : JavaDelegate {
     private val logger = LoggerFactory.getLogger(SaveSuggestionDelegate::class.java)
 
     override fun execute(delegateExecution: DelegateExecution?) {
         logger.info("Current activity: " + (delegateExecution!!.currentActivityName) + "...")
 
-        //TODO - Christof: as soon as we can retrieve Camunda variable you can uncomment the lines
-//        val camundaVariableHelper = CamundaVariableHelper(delegateExecution)
-//        val title = camundaVariableHelper[CamundaVariables.TITLE]
-//        val category = camundaVariableHelper.getOrNull(CamundaVariables.CATEGORY)
-//        val description = camundaVariableHelper.getOrNull(CamundaVariables.DESCRIPTION)
-//        val url = camundaVariableHelper.getOrNull(CamundaVariables.URL)
-//
-//        logger.info("\tSave:")
-//        logger.info("\t\tTitle: $title")
-//        logger.info("\t\tCategory: $category")
-//        logger.info("\t\tDescription: $description")
-//        logger.info("\t\tURL: $url")
+        val title = delegateExecution.getVariable("title")
+        val category = delegateExecution.getVariable("category")
+        val description = delegateExecution.getVariable("description")
+        val url = delegateExecution.getVariable("url")
+        val comments = delegateExecution.getVariable("comments")
+
+        logger.info("\tSave:")
+        logger.info("\t\tTitle: $title")
+        logger.info("\t\tCategory: $category")
+        logger.info("\t\tDescription: $description")
+        logger.info("\t\tURL: $url")
+        logger.info("\t\tComments from software architect: $comments")
     }
 }
 
