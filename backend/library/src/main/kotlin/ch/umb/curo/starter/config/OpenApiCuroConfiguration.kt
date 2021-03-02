465d9c29b2cdb83fdcacdf27fe2608c6d36f0ccf
itsmefox <patrick.boesch@umb.ch>
2021-03-02 15:15:35 +0100

    fix: change to correct conditional on class (#125)

--- backend/library/src/main/kotlin/ch/umb/curo/starter/config/OpenApiCuroConfiguration.kt
+++ backend/library/src/main/kotlin/ch/umb/curo/starter/config/OpenApiCuroConfiguration.kt
@@ -5,13 +5,14 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springdoc.core.GroupedOpenApi
+import org.springdoc.webmvc.ui.SwaggerConfig
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConditionalOnClass(OpenAPI::class)
-open class OpenApiConfiguration {
+@ConditionalOnClass(SwaggerConfig::class)
+open class OpenApiCuroConfiguration {
 
     @Bean
     open fun publicApi(): GroupedOpenApi {
